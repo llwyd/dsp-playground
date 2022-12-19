26c9eb43bee683ec4871ee42a41f8c11eb2ba5a4
llwyd <tom@llwyd.io>
2022-12-19 18:56:58 +0000

    added more references

--- cascade.py
+++ cascade.py
@@ -3,6 +3,27 @@ import matplotlib.pyplot as plt
 from scipy import signal
 from scipy.io import wavfile
 
+def pink_filter():
+	# Taken from http://www.firstpr.com.au/dsp/pink-noise/
+	# Filter design by Robert Bristow-Johnson
+	p = [0.99572754,0.94790649,0.53567505] 
+	z = [0.98443604,0.83392334,0.07568359] 
+	k = 1
+
+	# This converts to poles and zeros into second order sections
+	sos = signal.zpk2sos( z, p, k );
+	
+	return sos
+
+def voss_algorithm( bands, n ):
+	pink = np.zeros(n)
+	noise = np.zeros(bands)
+	
+	for i in range(n):
+		pink[i] = 0
+		for j in range(bands):
+			if((i+1)%np.power(2,j)==0):
+				noise[j]=(r.random()*2)-1;
 
 def fft(x,fs,fft_len):
     F = np.fft.fft(x,fft_len)
@@ -40,8 +61,7 @@ sig_len = 8192 * 8
 [wav_fs, wav_pink] = wavfile.read("pink.wav")
 wav_pink = norm(wav_pink) * gain ( -50 )
 
-
-cutoff = [4, 32, 256, 2048, 16384 ]
+cutoff = [16, 128, 1024, 4096 ]
 current_gain = -6
 
 lpf = []
@@ -50,6 +70,9 @@ for i in range( len( cutoff ) ):
     current_gain = current_gain -6
 
 dirac = signal.unit_impulse(sig_len)
+
+p = signal.sosfilt( pink_filter(), dirac )
+p = norm( p ) * gain( -27)
 
 x = []
 for i in range( len( cutoff ) ):
@@ -61,14 +84,20 @@ for i in range( len( cutoff ) ):
     y = y + x[i]
 
 
-Y, Yf, Ydb = fft( y, fs, sig_len)
-P, Pf, Pdb = fft( wav_pink, wav_fs, len(wav_pink))
+ref  = [ 0, -10, -20, -30, -40]
+reff = [ 10, 100, 1000, 10000, 100000]
 
+Y, Yf, Ydb = fft( y, fs, sig_len)
+P, Pf, Pdb = fft( p, fs, sig_len)
+PINK, PINKf, PINKdb = fft( wav_pink, wav_fs, len(wav_pink))
+
+plt.semilogx( PINKf, PINKdb )
 plt.semilogx( Yf, Ydb )
-for i in range( len( cutoff ) ):
-    plt.semilogx( lpf[i].FFTf, lpf[i].FFTdb )
+#for i in range( len( cutoff ) ):
+#    plt.semilogx( lpf[i].FFTf, lpf[i].FFTdb )
 
 plt.semilogx( Pf, Pdb )
+plt.semilogx( reff, ref )
 plt.ylim( -150, 10 )
 plt.xlim( 1, int(fs / 2 ) )
 plt.grid(which='both')
