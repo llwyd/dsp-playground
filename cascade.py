7a753c5925dc1749162353cdcbaf24b7d0266a78
llwyd <tom@llwyd.io>
2022-12-19 17:44:44 +0000

    added pink noise ref to compare

--- cascade.py
+++ cascade.py
@@ -1,6 +1,7 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from scipy import signal
+from scipy.io import wavfile
 
 
 def fft(x,fs,fft_len):
@@ -22,6 +23,10 @@ fs = 48000
 order = 1
 sig_len = 8192 * 8
 
+[wav_fs, wav_pink] = wavfile.read("pink.wav")
+wav_pink = norm(wav_pink) * gain ( -50 )
+
+
 lpf_0 = signal.butter(order, 4,'lp',fs=fs,output='sos')
 lpf_1 = signal.butter(order, 16,'lp',fs=fs,output='sos')
 lpf_2 = signal.butter(order, 64,'lp',fs=fs,output='sos')
@@ -41,15 +46,15 @@ x_5 = signal.sosfilt(lpf_5, h ) * gain( -6 * 6)
 x_6 = signal.sosfilt(lpf_6, h ) * gain( -6 * 7)
 
 
-x = signal.sosfilt(lpf_0, h ) * gain( -6 * 1 ) 
-x = signal.sosfilt(lpf_1, x ) * gain( -6 * 2 ) 
-x = signal.sosfilt(lpf_2, x ) * gain( -6 * 3 ) 
-x = signal.sosfilt(lpf_3, x ) * gain( -6 * 4 ) 
-x = signal.sosfilt(lpf_4, x ) * gain( -6 * 5 ) 
-x = signal.sosfilt(lpf_5, x ) * gain( -6 * 6 ) 
-x = signal.sosfilt(lpf_6, x ) * gain( -6 * 7 ) 
+y_0 = signal.sosfilt(lpf_0, h ) * gain( -6 * 1 ) 
+y_1 = signal.sosfilt(lpf_1, h ) * gain( -6 * 2 ) 
+y_2 = signal.sosfilt(lpf_2, h ) * gain( -6 * 3 ) 
+y_3 = signal.sosfilt(lpf_3, h ) * gain( -6 * 4 ) 
+y_4 = signal.sosfilt(lpf_4, h ) * gain( -6 * 5 ) 
+y_5 = signal.sosfilt(lpf_5, h ) * gain( -6 * 6 ) 
+y_6 = signal.sosfilt(lpf_6, h ) * gain( -6 * 7 ) 
 
-x = x* gain( 6 * 25 )
+x = y_0 + y_1 + y_2 + y_3 + y_4 + y_5 + y_6
 
 X_0, Xf_0, Xdb_0 = fft( x_0, fs, sig_len)
 X_1, Xf_1, Xdb_1 = fft( x_1, fs, sig_len)
@@ -59,6 +64,7 @@ X_4, Xf_4, Xdb_4 = fft( x_4, fs, sig_len)
 X_5, Xf_5, Xdb_5 = fft( x_5, fs, sig_len)
 X_6, Xf_6, Xdb_6 = fft( x_6, fs, sig_len)
 X, Xf, Xdb = fft( x, fs, sig_len)
+P, Pf, Pdb = fft( wav_pink, wav_fs, len(wav_pink))
 
 plt.semilogx( Xf, Xdb )
 plt.semilogx( Xf_0, Xdb_0 )
@@ -68,6 +74,7 @@ plt.semilogx( Xf_3, Xdb_3 )
 plt.semilogx( Xf_4, Xdb_4 )
 plt.semilogx( Xf_5, Xdb_5 )
 plt.semilogx( Xf_6, Xdb_6 )
+plt.semilogx( Pf, Pdb )
 plt.ylim( -150, 10 )
 plt.xlim( 1, int(fs / 2 ) )
 plt.grid(which='both')
