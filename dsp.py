6ab924257700f0b63815ef91f91e3eecc089a381
llwyd <tom@llwyd.io>
2022-12-30 18:49:13 +0000

    new ewma class

--- dsp.py
+++ dsp.py
@@ -17,6 +17,18 @@ class LPF():
 
         self.FFT, self.FFTf, self.FFTdb = fft( self.ir, self.fs, self.sig_len )
 
+class SinglePoleLPF():
+    def __init__( self, order, cutoff, raw_gain, fs, sig_len ):
+        self.fs = fs
+        self.cutoff = cutoff
+        self.order = order
+        self.gain = raw_gain
+        self.sig_len = sig_len
+
+        self.ir = ewma( self.cutoff, self.sig_len) * gain( raw_gain )
+
+        self.FFT, self.FFTf, self.FFTdb = fft( self.ir, self.fs, self.sig_len )
+
 def fft(x,fs,fft_len,norm=None):
     F = np.fft.fft(x,fft_len,norm=norm)
     F = np.abs(F)
