8a60dc0cfd089b6d2f221e5cb0b244c406429a57
llwyd <tom@llwyd.io>
2022-12-30 22:12:12 +0000

    update of single band working

--- dsp.py
+++ dsp.py
@@ -26,9 +26,14 @@ class SinglePoleLPF():
         self.gain = raw_gain
         self.sig_len = sig_len
 
-        self.ir = ewma( self.alpha, self.sig_len) * gain( raw_gain )
-
+        self.ir = ewma( self.alpha, self.sig_len) * gain( self.gain )
         self.FFT, self.FFTf, self.FFTdb = fft( self.ir, self.fs, self.sig_len )
+
+    def update_gain( self, raw_gain ):
+        self.gain = raw_gain
+        self.ir = ewma( self.alpha, self.sig_len) * gain( self.gain )
+        self.FFT, self.FFTf, self.FFTdb = fft( self.ir, self.fs, self.sig_len )
+
 
 def fft(x,fs,fft_len,norm=None):
     F = np.fft.fft(x,fft_len,norm=norm)
--- interactive_cascade.py
+++ interactive_cascade.py
@@ -5,8 +5,28 @@ from scipy import signal
 from scipy.io import wavfile
 import dsp
 
+
+
+def update_gain_0(val):
+    print(axband[0].val)
+    lpf[0].update_gain(axband[0].val)
+    Y_plot.set_ydata( lpf[0].FFTdb )
+
+def update_plot():
+    pass
+
+def stringify( val ):
+    return str(val) + " Hz "
+
 fs = 48000
-sig_len = 8192
+sig_len = 8192 * 8
+
+bands = [1, 10, 100, 1000, 10000]
+ideal_db, ideal_f = dsp.generate_decade_line( 20, 100000 )
+lpf = []
+for cutoff in bands:
+    lpf.append( dsp.SinglePoleLPF( 1, cutoff, 1, fs, sig_len ) )
+
 
 fig, ax = plt.subplots()
 fig.subplots_adjust(bottom=0.35)
@@ -14,11 +34,29 @@ ax.set_xlabel('Frequency (Hz)')
 ax.set_ylabel('Magnitude (dB)')
 axcolor = 'lightgoldenrodyellow'
 
+Y_plot, = ax.semilogx( lpf[0].FFTf, lpf[0].FFTdb )
+ideal, = ax.semilogx(ideal_f, ideal_db )
+
+
 axcolor = 'lightgoldenrodyellow'
 axfreq = []
 axband = []
 
-axfreq.append(fig.add_axes([0.2, 0.035, 0.03, 0.20], facecolor=axcolor))
-axband.append( Slider(axfreq[0],'1 Hz', -50, 20, valinit=1,valstep=1,orientation = "vertical" ) )
+slider_height = 0.2
+slider_width = 0.03
+slider_pos_x = 0.2
+slider_pos_y = 0.035
+slider_pos_x_inc = 0.035
+
+for i in range(len(bands)):
+    axfreq.append(fig.add_axes([slider_pos_x, slider_pos_y, slider_width, slider_height], facecolor=axcolor))
+    axband.append( Slider(axfreq[i],stringify(bands[i]), -50, 20, valinit=1,valstep=1,orientation = "vertical" ) )
+    slider_pos_x += slider_pos_x_inc
+
+axband[0].on_changed(update_gain_0)
+axband[1].on_changed(update_gain_0)
+axband[2].on_changed(update_gain_0)
+axband[3].on_changed(update_gain_0)
+axband[4].on_changed(update_gain_0)
 
 plt.show()
