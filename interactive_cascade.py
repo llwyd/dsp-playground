7bdce24f5f6ad83413d7ce6a1c0d50e2ef15284b
llwyd <tom@llwyd.io>
2022-12-30 21:27:54 +0000

    beginnings of user interface

--- interactive_cascade.py
+++ interactive_cascade.py
@@ -5,13 +5,20 @@ from scipy import signal
 from scipy.io import wavfile
 import dsp
 
-
 fs = 48000
 sig_len = 8192
 
 fig, ax = plt.subplots()
-plt.subplots_adjust(bottom=0.35)
-plt.xlabel('Frequency (Hz)')
-plt.ylabel('Magnitude (dB)')
+fig.subplots_adjust(bottom=0.35)
+ax.set_xlabel('Frequency (Hz)')
+ax.set_ylabel('Magnitude (dB)')
+axcolor = 'lightgoldenrodyellow'
+
+axcolor = 'lightgoldenrodyellow'
+axfreq = []
+axband = []
+
+axfreq.append(fig.add_axes([0.2, 0.035, 0.03, 0.20], facecolor=axcolor))
+axband.append( Slider(axfreq[0],'1 Hz', -50, 20, valinit=1,valstep=1,orientation = "vertical" ) )
 
 plt.show()
