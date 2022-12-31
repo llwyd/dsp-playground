389e2a54a1a4143f72a47779551804a5ec9579fd
llwyd <tom@llwyd.io>
2022-12-31 01:18:43 +0000

    some of this is not nice, trying to fix gain issues

--- interactive_cascade.py
+++ interactive_cascade.py
@@ -8,35 +8,30 @@ import dsp
 
 
 def update_gain_0(val):
-    print(axband[0].val)
     lpf[0].update_gain(axband[0].val)
     B0_plot.set_ydata( lpf[0].FFTdb )
     new_y = update_filter(lpf)
     update_graph(new_y)
 
 def update_gain_1(val):
-    print(axband[1].val)
     lpf[1].update_gain(axband[1].val)
     B1_plot.set_ydata( lpf[1].FFTdb )
     new_y = update_filter(lpf)
     update_graph(new_y)
 
 def update_gain_2(val):
-    print(axband[2].val)
     lpf[2].update_gain(axband[2].val)
     B2_plot.set_ydata( lpf[2].FFTdb )
     new_y = update_filter(lpf)
     update_graph(new_y)
 
 def update_gain_3(val):
-    print(axband[3].val)
     lpf[3].update_gain(axband[3].val)
     B3_plot.set_ydata( lpf[3].FFTdb )
     new_y = update_filter(lpf)
     update_graph(new_y)
 
 def update_gain_4(val):
-    print(axband[4].val)
     lpf[4].update_gain(axband[4].val)
     B4_plot.set_ydata( lpf[4].FFTdb )
     new_y = update_filter(lpf)
@@ -47,11 +42,19 @@ def update_plot():
 
 def update_filter(lpf):
     y = np.zeros(sig_len)
-    total_gain = 1
+    total_gain = 0
     for i in range( len(lpf ) ):
         y = y + lpf[i].ir
-        total_gain += lpf[i].gain
-    y = y * dsp.gain( total_gain)
+        print("\t"+ str(np.max(np.abs(lpf[i].ir))))
+        total_gain += dsp.gain( lpf[i].gain )
+    print("Total gain: " + str(total_gain) )
+
+    raw_gain = -20*np.log10(total_gain)
+    print("Raw: " + str(raw_gain))
+
+    y = y * dsp.gain(total_gain)
+    max_val = np.max(np.abs(y))
+    print("Max Val: " +str(max_val))
     return y
 
 def update_graph(y):
@@ -68,7 +71,7 @@ bands = [1, 10, 100, 1000, 10000]
 ideal_db, ideal_f = dsp.generate_decade_line( 20, 100000 )
 lpf = []
 for cutoff in bands:
-    lpf.append( dsp.SinglePoleLPF( 1, cutoff, 1, fs, sig_len ) )
+    lpf.append( dsp.SinglePoleLPF( 1, cutoff, 0, fs, sig_len ) )
 
 
 fig, ax = plt.subplots()
@@ -101,7 +104,7 @@ slider_pos_x_inc = 0.035
 
 for i in range(len(bands)):
     axfreq.append(fig.add_axes([slider_pos_x, slider_pos_y, slider_width, slider_height], facecolor=axcolor))
-    axband.append( Slider(axfreq[i],stringify(bands[i]), -50, 20, valinit=1,valstep=1,orientation = "vertical" ) )
+    axband.append( Slider(axfreq[i],stringify(bands[i]), -50, 20, valinit=0,valstep=1,orientation = "vertical" ) )
     slider_pos_x += slider_pos_x_inc
 
 axband[0].on_changed(update_gain_0)
