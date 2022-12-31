6b114d14c709afa0a59d9c9733ff1bc754c52a24
llwyd <tom@llwyd.io>
2022-12-31 23:51:36 +0000

    updated white.py to experiment integer white noise generation

--- white.py
+++ white.py
@@ -1,38 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import noise
-
+import random
 
 fs = 48000
 sig_len = 4096
 
-white_u32 = noise.U32BitNoiseGenerator(seed=0x87654321)
-noise_u32 = noise.U32BitNoiseGenerator(seed=0x12345678)
+white_u32 = noise.U32BitNoiseGenerator(seed=random.getrandbits(32))
+noise_u32 = noise.U32BitNoiseGenerator(seed=random.getrandbits(32))
 
-y_u32 = np.zeros(sig_len,dtype=np.uint32)
-x_u32 = np.zeros(sig_len,dtype=np.uint32)
-z_u32 = np.zeros(sig_len,dtype=np.uint32)
+num_sources = 4
+shift = int(np.log2(num_sources))
+white32 = []
+for i in range(num_sources):
+    white32.append(noise.U32BitNoiseGenerator(seed=random.getrandbits(32)))
 
-y_u64 = np.zeros(sig_len, dtype=np.uint64)
 
+x32 = np.zeros((num_sources,sig_len),dtype=np.uint32)
+y64 = np.zeros(sig_len, dtype=np.uint64)
 
 for i in range(sig_len):
-    white_u32.Update()
-    noise_u32.Update()
-    x_u32[i] = noise_u32.value
-    y_u32[i] = white_u32.value
+    y64[i] = np.uint64(0)
+    for j in range(num_sources):
+        white32[j].Update()
+        x32[j][i] = white32[j].value
 
-    y_u64[i] = np.uint64(x_u32[i]) + np.uint64(y_u32[i])
-    y_shifted = y_u64[i] >> np.uint64(1)
+        y64[i] = y64[i] + x32[j][i]
 
-    z_u32[i] = np.uint32(y_shifted)
+y32 = y64 >> shift
 
-
+plt.figure(1)
 
 plt.subplot(2,1,1)
-plt.plot(y_u32)
-plt.plot(x_u32)
+for i in range(num_sources):
+    plt.plot(x32[i])
 plt.subplot(2,1,2)
-plt.plot(y_u64)
-plt.plot(z_u32)
+plt.plot(np.int32(y32))
+
+plt.figure(2)
+plt.plot(y64)
+plt.plot(y32)
 plt.show()
