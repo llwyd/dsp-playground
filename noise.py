cd98699e7fc0e264952ba2662d3218684fc1e9db
llwyd <tom@llwyd.io>
2022-12-31 18:46:32 +0000

    32bit noise generator

--- noise.py
+++ noise.py
@@ -9,3 +9,20 @@ class NoiseGenerator():
     def __init__(self):
         self.value = 0
         self.prev_value = 0
+
+def xorshift(x):
+    assert x > 0
+    x = np.uint32(x)
+    x ^= x << np.uint32(13);
+    x ^= x >> np.uint32(17);
+    x ^= x << np.uint32(5);
+    return np.uint32(x)
+
+class U32BitNoiseGenerator():
+    def Update(self):
+        self.prev_value = np.uint32(self.value)
+        self.value = np.uint32(xorshift(self.value))
+    def __init__(self,seed=np.uint32(0x12345678)):
+        assert seed > 0
+        self.value = np.uint32(xorshift(seed))
+        self.prev_value = 0
--- white.py
+++ white.py
@@ -6,12 +6,16 @@ import noise
 fs = 48000
 sig_len = 4096
 white = noise.NoiseGenerator()
+white_u32 = noise.U32BitNoiseGenerator()
 
 y = np.zeros(sig_len)
+y_u32 = np.zeros(sig_len,dtype=np.uint32)
 
 for i in range(sig_len):
     white.Update()
+    white_u32.Update()
     y[i] = white.value
+    y_u32[i] = white_u32.value
 
-plt.plot(y)
+plt.plot(y_u32)
 plt.show()
