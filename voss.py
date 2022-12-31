190324b98ace476f951d193ae8614f100af0c08f
llwyd <tom@llwyd.io>
2022-12-31 20:20:25 +0000

    32bit integer voss algorithm

--- voss.py
+++ voss.py
@@ -55,9 +55,51 @@ def voss(num_samples,generators):
     x = dsp.norm(x)
     return x, indices
 
+def voss32(num_samples,generators):
+    assert ( generators & (generators - 1) ) == 0
+    shift = np.uint32(np.log2(generators))
+    rollover = 2**( generators - 1 )
+    noise_array = []
+    
+    x = np.zeros(num_samples, dtype=np.uint32)
+    pink = np.uint64(0)
+
+    white_noise = noise.U32BitNoiseGenerator(seed=random.getrandbits(32))
+    white_noise.Update()
+
+    for i in range(generators):
+        noise_array.append(noise.U32BitNoiseGenerator(seed=random.getrandbits(32)))
+        noise_array[i].Update()
+        pink = pink + np.uint64( noise_array[i].value )
+
+    pink = pink + np.uint64( white_noise.value )
+    
+    counter = 1
+    indices = np.zeros(num_samples)
+    for i in range(num_samples):
+        index = trailing_bits(counter)
+        indices[i] = index
+    
+        noise_array[index].Update()
+        white_noise.Update()
+
+        pink = pink - np.uint64(white_noise.prev_value)
+        pink = pink + np.uint64(white_noise.value)
+
+        pink = pink - np.uint64(noise_array[index].prev_value);
+        pink = pink + np.uint64(noise_array[index].value);
+        x[i] = np.uint32(pink >> shift)
+
+        counter = ( counter & (rollover - 1) )
+        counter = counter + 1
+
+    assert np.max(np.abs(x))<= np.uint32(0xFFFFFFFF) 
+    return x, indices
+
+
 fs = 48000
 num_samples = 4096 * 4
-num_tests = 4
+num_tests = 32
 generators = 16
 
 print(f'Voss-McCartney Pink Noise Generator')
@@ -68,13 +110,20 @@ print(f'Test iterations: {num_tests}')
 print(f'Generating...')
 
 Ydb = np.zeros(int(num_samples/2))
+Ydb_32 = np.zeros(int(num_samples/2))
 Yf = []
+
 for i in trange(num_tests):
     x, indices = voss(num_samples,generators)
+    x_32, _ = voss32(num_samples,generators)
+    x_32 = dsp.norm(x_32)
     X, Xf, Xdb = dsp.fft(x, fs, len(x),norm='ortho' )
+    X_32, Xf_32, Xdb_32 = dsp.fft(x_32, fs, len(x_32),norm='ortho')
     Ydb = np.add(Ydb,Xdb)
+    Ydb_32 = np.add( Ydb_32, Xdb_32 )
 
 Zdb = Ydb / num_tests
+Zdb_32 = Ydb_32 / num_tests
 
 ideal_db, ideal_f = dsp.generate_decade_line(20, 100000)
 
@@ -85,6 +134,14 @@ plt.semilogx( ideal_f, ideal_db )
 
 plt.xlim( 1, int(fs / 2 ) )
 plt.grid(which='both')
+
+plt.figure(2)
+plt.semilogx( Xf_32, Xdb_32 )
+plt.semilogx( Xf_32, Zdb_32 )
+plt.semilogx( ideal_f, ideal_db )
+plt.xlim( 1, int(fs / 2 ) )
+plt.grid(which='both')
+
 #plt.figure(2)
 #plt.stem(indices)
 plt.show()
