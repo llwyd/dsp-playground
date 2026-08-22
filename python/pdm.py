b8965cbec0190326ab566ce5ffa015cf654e5fc7
llwyd <tom@llwyd.io>
2026-08-22 16:07:11 +0100

    pdm with speedy x64 sample clock

--- python/pdm.py
+++ python/pdm.py
@@ -1,28 +1,43 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-fs = 100
-f = 1
-t = np.linspace(0,fs - 1, fs)
+
+siglen_s = 0.1 # seconds of audio
+fs = 48000
+siglen = int(siglen_s * fs)
+
+f = 100
+t = np.linspace(0,siglen - 1, siglen)
+x_t = np.linspace(0,siglen_s, siglen)
 x = np.sin(2 * np.pi * f * t/fs)
 
+pdm_fs = 3072000
+pdm_siglen = int(pdm_fs * siglen_s)
+pdm = np.zeros(pdm_siglen)
+pdm_t = np.linspace(0, siglen_s,pdm_siglen)
+
+fs_ratio = int(pdm_fs / fs)
+
 v_ref = 1.0
-y = np.zeros(fs)
+y = np.zeros(siglen)
 integrator = 0
 comparator = 0
 output = 0
 dac_1_bit = [-v_ref,v_ref]
 
-for i in range(fs):
-    err = x[i] - output
-    integrator += err
-    if integrator > 0:
-        comparator = 1
-    else:
-        comparator = 0
-    y[i] = comparator
-    output = dac_1_bit[comparator]
+k = 0
+for i in range(siglen):
+    for j in range(fs_ratio):
+        err = x[i] - output
+        integrator += err
+        if integrator > 0:
+            comparator = 1
+        else:
+            comparator = 0
+        pdm[k] = comparator
+        output = dac_1_bit[comparator]
+        k += 1
 
-plt.plot((x + 1) / 2)
-plt.step(t,y)
+plt.plot(x_t,(x + 1) / 2)
+plt.step(pdm_t,pdm)
 plt.show()
