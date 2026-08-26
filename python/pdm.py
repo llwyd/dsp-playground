b6b2fe0eba0eb1176da2d25f38b329818343b953
llwyd <tom@llwyd.io>
2026-08-26 15:09:53 +0100

    changes for blog

--- python/pdm.py
+++ python/pdm.py
@@ -7,10 +7,11 @@ siglen_s = 0.1 # seconds of audio
 fs = 48000
 siglen = int(siglen_s * fs)
 
-f = 100
+v_ref = 1.65
+f = 1000
 t = np.linspace(0,siglen - 1, siglen)
 x_t = np.linspace(0,siglen_s, siglen)
-x = np.sin(2 * np.pi * f * t/fs)
+x = np.sin(2 * np.pi * f * t/fs) * v_ref
 
 pdm_fs = 3072000
 pdm_siglen = int(pdm_fs * siglen_s)
@@ -19,12 +20,11 @@ pdm_t = np.linspace(0, siglen_s,pdm_siglen)
 
 fs_ratio = int(pdm_fs / fs)
 
-v_ref = 1.0
 y = np.zeros(siglen)
 integrator = 0
 comparator = 0
 output = 0
-dac_1_bit = [-v_ref,v_ref]
+feedback = [-v_ref,v_ref]
 
 k = 0
 for i in range(siglen):
@@ -35,8 +35,8 @@ for i in range(siglen):
             comparator = 1
         else:
             comparator = 0
-        pdm[k] = comparator
-        output = dac_1_bit[comparator]
+        pdm[k] = comparator * v_ref
+        output = feedback[comparator]
         k += 1
 
 cutoff = fs / 2
@@ -54,4 +54,15 @@ plt.subplot(2,1,2)
 #plt.plot(x_t,(x + 1) / 2)
 plt.plot(x_t,x)
 plt.plot(x_t,z)
+
+plt.figure(2)
+plt.step(pdm_t,pdm)
+#plt.plot(x_t,(x + v_ref) / 2)
+plt.plot(x_t,x)
+plt.xlim(0,0.001)
+plt.ylim(-v_ref - 0.01, v_ref + 0.01)
+plt.legend(['PDM','Analogue signal'])
+plt.title('PDM encoding of 1kHz tone')
+plt.xlabel('Time (s)')
+plt.ylabel('Voltage (V)')
 plt.show()
